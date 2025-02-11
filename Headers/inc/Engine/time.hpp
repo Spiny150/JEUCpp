128bd262a50c29320f0ff28da6529e15b06696d6
spiny150 <matthias.lietard@outlook.fr>
2025-02-11 18:44:50 +0100

    feat: ajoute affichange des fps

--- Headers/inc/Engine/time.hpp
+++ Headers/inc/Engine/time.hpp
@@ -7,6 +7,11 @@ class Time
 private:
     static Uint64 lastFrameTime;
     static Uint64 currentFrameTime;
+
+    static int frameCount;
+    static Uint64 timeSinceLM; // LM = LastMeasurement
+    static int measurementFrequency;
+
 public:
     static double timeScale;
     static double deltaTime;
--- Sources/src/Engine/time.cpp
+++ Sources/src/Engine/time.cpp
@@ -1,4 +1,5 @@
 #include "Engine/time.hpp"
+#include <iostream>
 
 Uint64 Time::lastFrameTime = 0;
 Uint64 Time::currentFrameTime = SDL_GetPerformanceCounter();
@@ -6,9 +7,20 @@ double Time::unscaledDeltaTime = 0;
 double Time::deltaTime = 0;
 double Time::timeScale = 1;
 
+int Time::measurementFrequency = 1000;
+int Time::frameCount = 0;
+Uint64 Time::timeSinceLM = SDL_GetTicks64();
+
 void Time::Update() {
     lastFrameTime = currentFrameTime;
     currentFrameTime = SDL_GetPerformanceCounter();
     unscaledDeltaTime = (double) (currentFrameTime - lastFrameTime) / SDL_GetPerformanceFrequency();
     deltaTime = unscaledDeltaTime * timeScale;
+
+    frameCount++;
+    if (SDL_GetTicks64() > timeSinceLM + measurementFrequency) {
+        std::cout << "Fps : " << frameCount << std::endl;
+        frameCount = 0;
+        timeSinceLM = SDL_GetTicks64();
+    }
 }
